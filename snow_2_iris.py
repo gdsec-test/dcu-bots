67009f090c360e009834543f10a190a68233dfe6
abean <abean@godaddy.com>
2017-01-20 11:43:39 -0700

    implemented changes from code review

--- snow_2_iris.py
+++ snow_2_iris.py
@@ -7,6 +7,7 @@ import os
 from ConfigParser import SafeConfigParser
 from datetime import datetime
 from suds.client import Client
+from sys import exit
 
 mode = os.getenv('sysenv') or 'dev'
 
@@ -22,17 +23,8 @@ logging.basicConfig(level=logging.INFO)
 # enabling debugging for suds.client to show sent/received soap messages
 logging.getLogger('suds.client').setLevel(logging.INFO)
 
-# WSDL (web services description file) for IRIS Webservice is
-wsdl_url = settings.get('wsdl_url')
-
-# create a client for RegDBWbSvcService WSDL
-client = Client(wsdl_url)
-
-# Gets machine IP
-ip = socket.gethostbyname(socket.gethostname())
-
-# SNOW URL to obtain all open CHILD_ABUSE and CONTENT TICKET TYPES
-snow_url = settings.get('snow_url')
+# create a client for RegDBWbSvcService and get wsdl URL
+client = Client(settings.get('wsdl_url'))
 
 # SNOW CREDS
 snow_user = settings.get('snow_user')
@@ -41,20 +33,17 @@ snow_pass = settings.get('snow_pass')
 # Set proper SNOW request headers headers
 headers = {"Content-Type": "application/json", "Accept": "application/json"}
 
-# Do the HTTP request to SNOW
-response = requests.get(snow_url, auth=(snow_user, snow_pass), headers=headers)
-
 
 # Create a list of tickets that need to be sent to IRIS.  Returns tuple of two lists
 # Also creates list of SNOW ticket numbers and sys_ids of tickets being sent to IRIS so they can be closed in SNOW
 # Tickets translated to more readable dict with only necessary data
-def data_for_iris():
+def data_for_iris(snow_response):
     # List of the ticket dictionaries from dict_for_iris.  Will be sent to IRIS in send_to_iris method
     list_for_iris = []
     # List of the ticket dictionaries from dict_for_closing.  Will be used in close_snow_tickets method
     list_for_closing = []
-    for child in data['result']:
-        # Dictionary containing only the specified ticket data for a single ticket from data.  Each pass is another ticket
+    for child in snow_response['result']:
+        # Dictionary containing only the specified ticket data for a single ticket from data.Each pass is another ticket
         dict_for_iris = {'snowTicket': child['u_number'],
                          'type': child['u_type'],
                          'source': child['u_source'],
@@ -67,14 +56,14 @@ def data_for_iris():
                          }
         list_for_iris.append(dict_for_iris)
 
-        # Dictionary containing only the specified ticket data for a single ticket from data.  Each pass is another ticket
+        # Dictionary containing only the specified ticket data for a single ticket from data.Each pass is another ticket
         dict_for_closing = {'tix_num': child['u_number'], 'sys_id': child['sys_id']}
         list_for_closing.append(dict_for_closing)
 
-    return (list_for_iris, list_for_closing)
+    return list_for_iris, list_for_closing
 
 
-# Accepts list_for_closing from data_for_iris method.  Closes SNOW tickets.  Returns a list of messages from ticket closing
+# Accepts list_for_closing from data_for_iris method.Closes SNOW tickets. Returns a list of messages from ticket closing
 def close_snow_tickets(list_for_closing):
     messages = []
     for child in list_for_closing:
@@ -99,111 +88,117 @@ def close_snow_tickets(list_for_closing):
     return messages
 
 
+# Method to make SOAP call to IRIS using variable data
+def create_iris_incident(subscriber_id, subject, note, group_id, service_id):
+
+    # Soap UI template used in send_to_iris() above
+    #
+    # Subscriber ID, Subject, Note, Customer Email Address, IP, Group ID, Service ID, Private Label ID, Shopper ID,
+    # Created By, Incident Type
+    #
+    # result = client.service.CreateIncidentInIRISByType('Subscriber ID', 'Subject', Note, 'Customer Email Address', IP,
+    #                                                     'Group ID', 'Service ID', 'Private Label ID', 'Shopper ID',
+    #                                                     'Created By', 'Incident Type')
+    #
+    # XXXXXXCreateIncidentInIRISByTypeXXXXXX
+    #
+    #       <tem:CreateIncidentInIRISByType>
+    #          <tem:subscriberID>425</tem:subscriberID>
+    #          <!--Optional:-->
+    #          <tem:subject>Test Subject</tem:subject>
+    #          <!--Optional:-->
+    #          <tem:Note>PAYLOAD</tem:Note>
+    #          <!--Optional:-->
+    #          <tem:customerEmailAddress>lhalvorson@godaddy.com</tem:customerEmailAddress>
+    #          <!--Optional:-->
+    #          <tem:originalIPAddress>?</tem:originalIPAddress>
+    #          <tem:groupID>510</tem:groupID>
+    #          <tem:serviceID>214</tem:serviceID>
+    #          <tem:privateLabelID>1</tem:privateLabelID>
+    #          <!--Optional:-->
+    #          <tem:shopperID></tem:shopperID>
+    #          <!--Optional:-->
+    #          <tem:createdBy>DCU-Eng</tem:createdBy>
+    #          <tem:incidentType>26</tem:incidentType>
+    #       </tem:CreateIncidentInIRISByType>
+
+    # Gets machine IP
+    ip = socket.gethostbyname(socket.gethostname())
+    # IRIS insert statement with proper Group/Service IDs
+    result = client.service.CreateIncidentInIRISByType(subscriber_id, subject, note, 'dcu@godaddy.com', ip, group_id,
+                                                       service_id, '1', '', 'DCU Eng Bot', '26')
+
+    return result
+
+
 # Accepts first item in the tuple from data_for_iris.  Returns a list of ticket numbers that were created in IRIS.
 # Method to make SOAP calls to create IRIS tickets for CONTENT or CHILD_ABUSE complaints originally submitted to SNOW
-def send_to_iris(snow_tickets):
+def send_to_iris(iris_list):
     messages = []
-    for i in snow_tickets:
+    for i in iris_list:
+        ticket_info = '\n'.join('{}: {}'.format(key, val) for key, val in i.items())
         if i['type'] == 'CONTENT':
-            ticket_info = '\n'.join('{}: {}'.format(key, val) for key, val in i.items())
+            content_subject = 'Content Complaints Report'
+
             if mode == 'prod':
                 # PROD IRIS insert statement with proper Group/Service IDs for PROD Environment for CONTENT type
-                result = client.service.CreateIncidentInIRISByType('436', 'Content Complaints Report', ticket_info,
-                                                                   'dcu@godaddy.com', ip, '432', '241', '1', '',
-                                                                   'DCU Eng Bot', '26')
+                iris_ticket = create_iris_incident('436', content_subject, ticket_info, '432', '241')
+
             else:
                 # DEV IRIS insert statement with proper Group/Service IDs for DEV Environment for CONTENT type
-                result = client.service.CreateIncidentInIRISByType('425', 'Content Complaints Report', ticket_info,
-                                                                   'dcu@godaddy.com', ip, '504', '227', '1', '',
-                                                                   'DCU Eng Bot', '26')
-            # print result
-            messages.append(result)
-        else:
-            ticket_info = '\n'.join('{}: {}'.format(key, val) for key, val in i.items())
+                iris_ticket = create_iris_incident('425', content_subject, ticket_info, '504', '227')
+
+            messages.append(iris_ticket)
+        elif i['type'] == 'CHILD_ABUSE':
+            child_abuse_subject = 'Child Abuse Report'
+
             if mode == 'prod':
                 # PROD IRIS insert statement with proper Group/Service IDs for PROD Environment for CHILD_ABUSE type
-                result = client.service.CreateIncidentInIRISByType('389', 'Child Abuse Report', ticket_info,
-                                                                   'dcu@godaddy.com', ip, '443', '221', '1', '',
-                                                                   'DCU Eng Bot', '26')
+                iris_ticket = create_iris_incident('389', child_abuse_subject, ticket_info, '443', '221')
+
             else:
                 # DEV IRIS insert statement with proper Group/Service IDs for DEV Environment for CHILD_ABUSE type
-                result = client.service.CreateIncidentInIRISByType('425', 'Child Abuse Report', ticket_info,
-                                                                   'dcu@godaddy.com', ip, '510', '214', '1', '',
-                                                                   'DCU Eng Bot', '26')
-            # print result
-            messages.append(result)
+                iris_ticket = create_iris_incident('425', child_abuse_subject, ticket_info, '510', '214')
+
+            messages.append(iris_ticket)
 
     return messages
+
 
 # Method to send method response to an alert channel for DCU
 def write_to_slack(iris_tickets, closed_tickets, slack_url):
     channel = settings.get('slack_channel')
+    message = ''
     if len(iris_tickets):
         message = '<!here> {} IRIS tickets created'.format(len(iris_tickets)) + '\n'
         for i in iris_tickets:
             message += str(i) + '\n'
-        payload = {'payload': json.dumps({
-            'channel': channel,
-            'username': 'DCU Eng BOT',
-            'text': message
-        })
-        }
-        requests.post(slack_url, data=payload)
 
     if len(closed_tickets):
-        message = '<!here> {} SNOW tickets closed'.format(len(closed_tickets)) + '\n'
+        message += '<!here> {} SNOW tickets closed'.format(len(closed_tickets)) + '\n'
         for i in closed_tickets:
             message += str(i) + '\n'
-        payload = {'payload': json.dumps({
-            'channel': channel,
-            'username': 'DCU Eng BOT',
-            'text': message
-        })
-        }
-        requests.post(slack_url, data=payload)
+    payload = {'payload': json.dumps({
+        'channel': channel,
+        'username': 'DCU Eng BOT',
+        'text': message
+    })
+    }
+    requests.post(slack_url, data=payload)
 
+# Do the HTTP request to SNOW
+response = requests.get(settings.get('snow_url'), auth=(snow_user, snow_pass), headers=headers)
 
 # Check for HTTP response codes from SNOW for other than 200
 if response.status_code != 200:
-    print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
+    logging.error('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
     exit()
+
 
 # Decode the SNOW JSON response into a dictionary and use the data
 data = response.json()
 
-# print data
-snow_tickets = data_for_iris()
+snow_tickets = data_for_iris(data)
 send_tickets = send_to_iris(snow_tickets[0])
 messages_from_closing = close_snow_tickets(snow_tickets[1])
 write_to_slack(send_tickets, messages_from_closing, settings.get('slack_url'))
-'''
-Soap UI template used in send_to_iris() above
-
-Subscriber ID, Subject, Note, Customer Email Address, IP, Group ID, Service ID, Private Label ID, Shopper ID, Created By, Incident Type
-
-result = client.service.CreateIncidentInIRISByType('Subscriber ID', 'Subject', Note, 'Customer Email Address', IP,
-                                                    'Group ID', 'Service ID', 'Private Label ID', 'Shopper ID',
-                                                    'Created By', 'Incident Type')
-
-XXXXXXCreateIncidentInIRISByTypeXXXXXX
-
-      <tem:CreateIncidentInIRISByType>
-         <tem:subscriberID>425</tem:subscriberID>
-         <!--Optional:-->
-         <tem:subject>Test Subject</tem:subject>
-         <!--Optional:-->
-         <tem:Note>PAYLOAD</tem:Note>
-         <!--Optional:-->
-         <tem:customerEmailAddress>lhalvorson@godaddy.com</tem:customerEmailAddress>
-         <!--Optional:-->
-         <tem:originalIPAddress>?</tem:originalIPAddress>
-         <tem:groupID>510</tem:groupID>
-         <tem:serviceID>214</tem:serviceID>
-         <tem:privateLabelID>1</tem:privateLabelID>
-         <!--Optional:-->
-         <tem:shopperID></tem:shopperID>
-         <!--Optional:-->
-         <tem:createdBy>DCU-Eng</tem:createdBy>
-         <tem:incidentType>26</tem:incidentType>
-      </tem:CreateIncidentInIRISByType>
-'''
